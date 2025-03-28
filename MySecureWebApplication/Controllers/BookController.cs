454f2b1384c67666cfb0453648ed21ef79d2e519
Ulisses Telemaco Neto <ulisses.telemaco@gmail.com>
2025-03-28 09:34:20 -0400

    Inclusing BookController and almost fake actions: Index and Create (Get/Post)

---
+++ MySecureWebApplication/Controllers/BookController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySecureWebApplication.Controllers
+{
+    public class BookController : Controller
+    {
+
+        private static List<string> Books;
+
+        public BookController()
+        {
+            if (Books == null)
+            {
+                Books = new List<string>();
+                Books.Add("Book 1");
+                Books.Add("Book 2");
+                Books.Add("Book 3");
+                Books.Add("Book 4");
+            }
+        }
+
+
+
+        // GET: HomeController1
+        public ActionResult Index()
+        {
+            return View(Books);
+        }
+
+        // GET: HomeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                Books.Add("Book " + (Books.Count + 1));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
